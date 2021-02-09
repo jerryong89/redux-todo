a656a3ad6b437b1a06a4b5f5816d370f89d73b0c
Jerry <jerryong89@gmail.com>
2021-02-09 10:19:13 +0000

    stopped at 31:54

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import './App.css';
 import Input from './components/Input.js';
 
+const todoList = [{
+  item: input,
+  done: false,
+  id: Date.now()
+}]
+
 function App() {
   return (
     <div className="app">
-      <Input/>
+      <div className="app_container">
+        <div className="app_todoContainer">
+
+        </div>
+        <Input/>
+      </div>
     </div>
   );
 }
