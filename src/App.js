2bd193276f44494e57b2958bf5bb79002b62de12
Jerry <jerryong89@gmail.com>
2021-02-10 04:14:03 +0000

    added material ui, hard coded todoitems

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import './App.css';
 import Input from './components/Input.js';
+import TodoItem from './components/TodoItem.js'
 
 const todoList = [{
-  item: input,
+  item: "todo",
   done: false,
-  id: Date.now()
-}]
+  id: 1234
+}, {
+  item: "todo2",
+  done: true,
+  id: 12345
+}
+]
 
 function App() {
   return (
     <div className="app">
       <div className="app_container">
         <div className="app_todoContainer">
-
+          {
+            todoList.map(item=>(
+              <TodoItem
+                name={item.item}
+                done={item.done}
+                id={item.id}
+              />
+            ))
+          }
         </div>
         <Input/>
       </div>
---
+++ src/components/TodoItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './TodoItem.css';
+import Checkbox from '@material-ui/core/Checkbox'
+
+const TodoItem = ({name, done, id}) => {
+    const handleCheck = () =>  {
+
+    }
+    return (
+        <div className='todoItem'>
+            <Checkbox
+                checked={done}
+                color="primary"
+                onChange={handleCheck}
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+            <p className={done && 'todoItem--done'}>{name}</p>
+        </div>
+    )
+}
+
+export default TodoItem
